45338ba3eda0966fed083ac49577ccd78548058d
Trương Gia Bảo <88610686+baotg080599@users.noreply.github.com>
2022-06-05 16:53:42 +0700

    Update generate_tfrecord.py

--- MiAI_SSD_TF2/generate_tfrecord.py
+++ MiAI_SSD_TF2/generate_tfrecord.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 import os
 import io
 import pandas as pd
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 from PIL import Image
 from object_detection.utils import dataset_util
